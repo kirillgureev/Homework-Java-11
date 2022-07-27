7068ff918a4ce2ec125c9d220c5f2b491d57e12c
Kirill Gureev <kir.gureev2010@ya.ru>
2022-07-27 23:13:08 +0300

    Second commit. Created classes and tests for ProductManager.

---
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -0,0 +1,38 @@
+package ru.netology.repository;
+
+import ru.netology.domain.Product;
+
+public class ProductRepository {
+    // Массив
+    private Product[] items = new Product[0];
+
+    // Методы
+    // 1. Сохранение Product
+    public void save(Product item) {
+        Product[] tmp = new Product[items.length + 1];
+        for (int i = 0; i < items.length; i++) {
+            tmp[i] = items[i];
+        }
+        tmp[tmp.length - 1] = item;
+        items = tmp;
+    }
+
+    // 2. Получение всех сохраненных Product
+    public Product[] findAll() {
+        return items;
+    }
+
+    // 3. Удаление по id
+    public void removeById(int id) {
+        Product[] tmp = new Product[items.length - 1];
+        int copyToIndex = 0;
+        for (Product item : items) {
+            if (item.getId() != id) {
+                tmp[copyToIndex] = item;
+                copyToIndex++;
+            }
+        }
+        items = tmp;
+    }
+
+}
